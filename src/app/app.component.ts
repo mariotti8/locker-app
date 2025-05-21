ba1e204823c149dd581e91cf0c23fefe5b73e97c
mariotti8 <mariotti93@gmail.com>
2025-05-21 18:52:55 +0200

    first commit 🚀

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// src/app/app.component.ts
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
+
+export enum LockerStatus {
+  APRI = 'Aperto',
+  CHIUDI = 'Chiuso',
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports: [CommonModule]
 })
 export class AppComponent {
-  title = 'locker-app';
+  subscription!: Subscription;
+  topicname: string = 'locker/1/status';
+  isConnected: boolean = false;
+  lockerStatus!: LockerStatus;
+
+  constructor(private _mqttService: MqttService) {
+    console.log('inside subscribe new topic');
+    this.subscription = this._mqttService
+      .observe(this.topicname)
+      .subscribe((message: IMqttMessage) => {
+        console.log('msg: ', message.payload.toString());
+        this.lockerStatus =
+          LockerStatus[message.payload.toString() as keyof typeof LockerStatus];
+      });
+  }
+
+  toggleLockerStatus() {
+    this.lockerStatus === LockerStatus.CHIUDI
+      ? this.sendmsg("APRI")
+      : this.sendmsg("CHIUDI");
+  }
+
+  get toggleText() {
+    return this.lockerStatus === LockerStatus.CHIUDI ? 'SBLOCCA' : 'BLOCCA';
+  }
+  get toggleIcon() {
+    return this.lockerStatus === LockerStatus.CHIUDI ? 'lock_open' : 'lock';
+  }
+
+  sendmsg(msg: string): void {
+    // use unsafe publish for non-ssl websockets
+    this._mqttService.unsafePublish(this.topicname, msg, {
+      qos: 1,
+      retain: true,
+    });
+  }
+
+  // apriLocker() {
+  //   this.mqttService.publish('locker/1/open', 'apri');
+  //   console.log('📤 Comando inviato: apri');
+  // }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { IMqttServiceOptions, MqttModule } from 'ngx-mqtt';
+
+const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'localhost',
+  port: 9001,
+  protocol: 'ws',
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(MqttModule.forRoot(MQTT_SERVICE_OPTIONS)),
+  ],
 };
